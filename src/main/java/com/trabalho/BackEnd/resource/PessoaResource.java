78e4943f51004eb248b4c17e84872d892030cb76
alan.brito <alan.brito@basis.com.br>
2018-08-30 13:26:06 -0300

    Criando requisições

--- src/main/java/com/trabalho/BackEnd/resource/PessoaResource.java
+++ src/main/java/com/trabalho/BackEnd/resource/PessoaResource.java
@@ -3,9 +3,8 @@ package com.trabalho.BackEnd.resource;
 import com.trabalho.BackEnd.model.Pessoa;
 import com.trabalho.BackEnd.service.PessoaService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,7 +16,29 @@ public class PessoaResource {
     private PessoaService pessoaService;
 
     @GetMapping
-    public List<Pessoa> finfAll(){
+    public List<Pessoa> findAll(){
         return pessoaService.findAll();
     }
+
+    @GetMapping("/{id}")
+    public Pessoa findById(@PathVariable Integer id){
+        return pessoaService.findById(id);
+    }
+
+    @PostMapping
+    public Pessoa save(@RequestBody Pessoa pessoa){
+        return pessoaService.save(pessoa);
+    }
+
+    @PutMapping("/{id}")
+    public Pessoa update(@PathVariable Integer id, @RequestBody Pessoa pessoa){
+        return pessoaService.update(id, pessoa);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable Integer id){
+        pessoaService.delete(id);
+    return ResponseEntity.noContent().build();
+    }
+
 }
--- src/main/java/com/trabalho/BackEnd/service/PessoaService.java
+++ src/main/java/com/trabalho/BackEnd/service/PessoaService.java
@@ -16,4 +16,30 @@ public class PessoaService {
     public List<Pessoa> findAll(){
         return this.pessoaRepository.findAll();
     }
+
+    public Pessoa findById(Integer id){
+        return this.pessoaRepository.findById(id).orElse(null);
+    }
+
+    public Pessoa save(Pessoa pessoa){
+        return this.pessoaRepository.save(pessoa);
+    }
+
+    public Pessoa update(Integer id, Pessoa pessoa){
+        Pessoa pessoaEncontrada = findById(id);
+        if (pessoaEncontrada != null){
+            pessoaEncontrada = pessoa;
+            pessoaEncontrada.setId(id);
+            return save(pessoaEncontrada);
+        }
+        return null;
+    }
+
+    public void delete(Integer id){
+        Pessoa pessoaEncontrada = findById(id);
+        if (pessoaEncontrada != null){
+            this.pessoaRepository.delete(pessoaEncontrada);
+        }
+    }
+
 }
